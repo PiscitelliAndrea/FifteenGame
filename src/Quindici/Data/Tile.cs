133266034e9757ca38e2b295117eb7630b0d1a96
Piscitelli Andrea <apiscitelli@progesoftware.it>
2020-08-12 22:27:56 +0200

    VS Solution added

---
+++ src/Quindici/Data/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quindici.Data
+{
+    public class Tile
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int number { get; set; }
+        public string backgroundColor { get; set; }
+
+        public string Number 
+        {
+            get
+            {
+                return number != 0 ? number.ToString() : "";
+            }
+            //set 
+            //{
+            //    number = Convert.ToInt32(value);
+            //} 
+        }
+    }
+}
---
+++ src/Quindici/Data/TilesGenerator.cs
@@ -0,0 +1,174 @@
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Quindici.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace Quindici.Data
+{
+    enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+
+    public class TilesGenerator
+    {
+        private static int MaxNumOfTiles = 16;
+        private List<Tile> TilesList = null;
+        public bool Done = false;
+
+        public List<Tile> GenerateTiles()
+        {
+            int row = 0;
+            int column = 0;
+            int nextRow = 3;
+
+            if (TilesList == null)
+            {
+                Random rnd = new Random();
+                int nextNum = 0;
+                TilesList = new List<Tile>();
+
+                for (int index = 0; index < MaxNumOfTiles; index++)
+                {
+                    var item = new Tile();
+                    item.Row = row;
+                    item.Column = column;
+
+                    do
+                    {
+                        nextNum = rnd.Next(0, 16);
+                    }
+                    while (TilesList.Where(n => n.number == nextNum).Any());
+
+                    item.number = nextNum;
+                    item.backgroundColor = ((nextNum % 2) == 0) ? "darkBackground" : "lightBackground";
+
+                    if (nextNum == 0)
+                        item.backgroundColor = "blackBackground";
+
+                    TilesList.Add(item);
+
+                    column += 1;
+                    if (column > nextRow)
+                    {
+                        column = 0;
+                        row += 1;
+                    }
+                }
+            }
+
+            return GetAllTiles();
+        }
+
+        private List<Tile> GetAllTiles()
+        {
+            List<Tile> SortedList = new List<Tile>();
+
+            SortedList = TilesList.OrderBy(n => n.Row).ThenBy(n => n.Column).ToList();
+
+            return SortedList;
+        }
+
+        public List<Tile> TryMoveTile(int row, int column)
+        {
+            // UP
+            if (row > 0
+                && TilesList.Where(n => (n.Row == (row - 1) && n.Column == column && n.number == 0)).Any())
+            {
+                MoveTile(row, column, Direction.Up);
+            }   // DOWN
+            else if (row < 3
+                && TilesList.Where(n => (n.Row == (row + 1) && n.Column == column && n.number == 0)).Any())
+            {
+                MoveTile(row, column, Direction.Down);
+            }   // LEFT
+            else if (column > 0
+                && TilesList.Where(n => (n.Row == row && n.Column == (column - 1) && n.number == 0)).Any())
+            {
+                MoveTile(row, column, Direction.Left);
+            }   // RIGHT
+            else if (column < 3
+                && TilesList.Where(n => (n.Row == row && n.Column == (column + 1) && n.number == 0)).Any())
+            {
+                MoveTile(row, column, Direction.Right);
+            }
+
+            return GetAllTiles();
+        }
+
+        private void MoveTile(int row, int column, Direction direction)
+        {
+            Tile tmpFrom = TilesList.Where(n => n.Row == row && n.Column == column).FirstOrDefault();
+            Tile tmpTo = new Tile();
+            Tile tmp = new Tile();
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    tmpTo = TilesList.Where(n => n.Row == (row - 1) && n.Column == column).FirstOrDefault();
+                    break;
+
+                case Direction.Down:
+                    tmpTo = TilesList.Where(n => n.Row == (row + 1) && n.Column == column).FirstOrDefault();
+                    break;
+
+                case Direction.Left:
+                    tmpTo = TilesList.Where(n => n.Row == row && n.Column == (column - 1)).FirstOrDefault();
+                    break;
+
+                case Direction.Right:
+                    tmpTo = TilesList.Where(n => n.Row == row && n.Column == (column + 1)).FirstOrDefault();
+                    break;
+            }
+
+            SwapTiles(tmpFrom, tmpTo);
+
+            this.Done = CheckSolution();
+        }
+
+        private bool CheckSolution()
+        {
+            int num = 1;
+            int counter = 1;
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 4; column++)
+                {
+                    num = TilesList.Where(n => n.Row == row && n.Column == column).FirstOrDefault().number;
+                    if (num != counter++)
+                        return false;
+
+                    if (counter > 15)
+                        counter = 0;
+                }
+            }
+
+            return true;
+        }
+
+        public List<Tile> Restart()
+        {
+            Done = false;
+            TilesList = null;
+            return GenerateTiles();
+        }
+
+        private void SwapTiles(Tile From, Tile To)
+        {
+            int value = From.number;
+            string background = From.backgroundColor;
+            From.number = 0;
+            From.backgroundColor = "blackBackground";
+            To.number = value;
+            To.backgroundColor = background;
+        }
+    }
+}
+
