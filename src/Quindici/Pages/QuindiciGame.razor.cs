8f7f2abd25a3e3eb56309ebdc8e5a0ab3f2139e7
Piscitelli Andrea <apiscitelli@progesoftware.it>
2020-08-13 23:32:49 +0200

    Added timer

--- src/Quindici/Pages/QuindiciGame.razor.cs
+++ src/Quindici/Pages/QuindiciGame.razor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Quindici.Data;
 
@@ -7,35 +9,93 @@ namespace Quindici.Pages
 {
     public partial class QuindiciGame
     {
+        #region Injection
+
         [Inject]
         TilesGenerator Tiles { get; set; }
 
+        #endregion
+
+        #region Internal properties
+
+        protected Timer timer { get; set; }
+        protected bool TimerStarted = false;
+
         protected List<Tile> tiles;
         protected Tile tile;
+
+        //private int ElapsedTime { get; set; }
+        private TimeSpan ElapsedTime = TimeSpan.FromMilliseconds(0);
+
+        #endregion
+
+        #region Life Cycle events
 
         protected override void OnInitialized()
         {
             tiles = Tiles.GenerateTiles();
         }
 
-        protected void ClickTile(int riga, int colonna)
+        #endregion
+
+        #region Tiles Methods
+
+        protected async Task ClickTile(int riga, int colonna)
         {
             if (!Tiles.Done)
             {
+                if (!TimerStarted)
+                {
+                    TimerStarted = true;
+                    StartCounter();
+                }
+
                 tiles = Tiles.TryMoveTile(riga, colonna);
-                RefreshPage();
+                if (Tiles.Done)
+                    StopCounter();
+                this.StateHasChanged();
             }
         }
 
         protected void Restart()
         {
+            ResetCounter();
+            TimerStarted = true;
+            StartCounter();
             tiles = Tiles.Restart();
-            RefreshPage();
-        }
-
-        protected void RefreshPage()
-        {
             this.StateHasChanged();
         }
+
+        #endregion
+
+        #region Timer Methods
+
+        void StartCounter()
+        {
+            timer = new Timer(new TimerCallback(_ =>
+            {
+                //ElapsedTime++;
+                ElapsedTime = ElapsedTime.Add(new TimeSpan(0, 0, 1));
+
+                // Note that the following line is necessary because otherwise
+                // Blazor would not recognize the state change and not refresh the UI
+                InvokeAsync(() =>
+                {
+                    StateHasChanged();
+                });
+            }), null, 1000, 1000);
+        }
+
+        void ResetCounter()
+        {
+            ElapsedTime = new TimeSpan(0, 0, 0);
+        }
+
+        void StopCounter()
+        {
+            timer.Dispose();
+        }
+
+        #endregion
     }
 }
