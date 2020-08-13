b569f46ae66d218e9f99dd198774febf104c5de2
Piscitelli Andrea <apiscitelli@progesoftware.it>
2020-08-13 22:22:39 +0200

    Code behind split

---
+++ src/Quindici/Pages/QuindiciGame.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Quindici.Data;
+
+namespace Quindici.Pages
+{
+    public partial class QuindiciGame
+    {
+        [Inject]
+        TilesGenerator Tiles { get; set; }
+
+        protected List<Tile> tiles;
+        protected Tile tile;
+
+        protected override void OnInitialized()
+        {
+            tiles = Tiles.GenerateTiles();
+        }
+
+        protected void ClickTile(int riga, int colonna)
+        {
+            if (!Tiles.Done)
+            {
+                tiles = Tiles.TryMoveTile(riga, colonna);
+                RefreshPage();
+            }
+        }
+
+        protected void Restart()
+        {
+            tiles = Tiles.Restart();
+            RefreshPage();
+        }
+
+        protected void RefreshPage()
+        {
+            this.StateHasChanged();
+        }
+    }
+}
